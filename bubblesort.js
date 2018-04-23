a7966aaf511b565962fb856d755bf9573cab7da6
Jami Gibbs <jami0821@gmail.com>
2018-04-23 11:59:01 -0500

    Sorting

--- bubblesort.js
+++ bubblesort.js
@@ -1,7 +1,8 @@
 function bubbleSort(arr) {
-  if (arr.length === 1) {
-    return arr[0];
-  } else {
+  const counter = arr.length;
+  let ticks = 0;
+
+  do {
     for (let i = 0; i < arr.length - 1; i++) {
       if (arr[i] > arr[i + 1]) {
         // swapping elements
@@ -9,10 +10,10 @@ function bubbleSort(arr) {
         arr[i + 1] = arr[i];
         arr[i] = temp;
       }
-      console.log('arr: ', arr);
     }
-    return bubbleSort(arr.slice(0, arr.length - 1));
-  }
+    ticks++;
+  } while (ticks < counter);
 
-  // return arr;
+  console.log(arr)
+  return arr;
 }
