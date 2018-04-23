be1573d6482df7da1ab6cf578e5eb69fc58aac8b
Dustin McDowell <dustinmcdowell1989@gmail.com>
2018-04-23 11:27:01 -0500

    started implementing bubble sort

--- bubblesort.js
+++ bubblesort.js
@@ -1,3 +1,18 @@
-function bubbleSort(arr){
-  return arr;
+function bubbleSort(arr) {
+  if (arr.length === 1) {
+    return arr[0];
+  } else {
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        // swapping elements
+        const temp = arr[i + 1];
+        arr[i + 1] = arr[i];
+        arr[i] = temp;
+      }
+      console.log('arr: ', arr);
+    }
+    return bubbleSort(arr.slice(0, arr.length - 1));
+  }
+
+  // return arr;
 }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,22 +1,21 @@
-describe('Bubble Sort', function(){
-
-  it('calls itself n times (recursive)', function(){
-    const arr = [5, 3, 7, 8, 10];
+describe('Bubble Sort', function() {
+  it('calls itself n times (recursive)', function() {
+    const arr = [5, 3, 10, 2, 8, 7];
     spyOn(window, 'bubbleSort').and.callThrough();
     bubbleSort(arr);
     expect(bubbleSort.calls.count()).toEqual(arr.length);
-  })
+  });
 
-  it('handles empty array', function(){
-    expect( bubbleSort([]) ).toEqual( [] );
+  it('handles empty array', function() {
+    expect(bubbleSort([])).toEqual([]);
   });
-  it('handles non-array argumnet', function(){
-    expect( bubbleSort('hello world') ).toEqual( undefined );
+  it('handles non-array argumnet', function() {
+    expect(bubbleSort('hello world')).toEqual(undefined);
   });
-  it('sorts array from lowest to highest', function(){
-    expect( bubbleSort([4,5,10,3,1]) ).toEqual( [1,3,4,5,10] );
+  it('sorts array from lowest to highest', function() {
+    expect(bubbleSort([4, 5, 10, 3, 1])).toEqual([1, 3, 4, 5, 10]);
   });
-  it('handles single element array', function(){
-    expect( bubbleSort([4]) ).toEqual( [4] );
+  it('handles single element array', function() {
+    expect(bubbleSort([4])).toEqual([4]);
   });
-})
+});
