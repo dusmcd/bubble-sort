d9093892ff365094609f745c417491f9254746e3
Mary Warrick <mewarrick@gmail.com>
2018-04-23 14:37:40 -0500

    first attempt on merge

--- mergesort.js
+++ mergesort.js
@@ -5,3 +5,26 @@ function split(wholeArray) {
 
   return [firstHalf, secondHalf];
 }
+
+function merge (arr1, arr2 ){
+  let newArray = [];
+
+  arr1.forEach(function(num, i){
+    if(num > arr2[i]){
+      newArray.push(arr2[i]);
+      newArray.push(num);
+    }else{
+      newArray.push(num);
+      newArray.push(arr2[i]);
+    }
+  })
+  return newArray;
+}
+
+
+
+// let extra =
+
+
+arr1 = arr1 >= arr2 ? arr2: arr1;
+//   let longArr = arr1 >= arr2 ? arr1: arr2;
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -10,3 +10,13 @@ describe('Split array function', function() {
     expect(split([1, 2, 3, 4, 5])).toEqual([[1, 2], [3, 4, 5]]);
   });
 });
+
+describe('Merge array function', function(){
+  it('takes two sorted arrays, and merges them into one sorted array' ,
+  function() {
+    expect(merge([7, 8, 10], [13, 15 ,16])).toEqual([7, 8,10, 13, 15, 16])
+  });
+  it('sorts large range', function() {
+    expect(merge([9, 35, 90], [2, 40, 75])).toEqual([2, 9, 35, 40, 75, 90])
+  })
+});
